1836d6cccc75e319524cd5ef57b63b3505577927
Арсен <arsendag505@gmail.com>
2023-11-12 00:16:08 +0300

    comma

--- build/main.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import ejs from 'ejs';
-import { router } from './modules/user-router.js';
-const app = express();
-const PORT = process.env.PORT || 3000;
-app.engine('html', ejs.renderFile);
-app.use(router);
-app.listen(PORT, () => console.log(`Listen on PORT ${PORT}`));
-//# sourceMappingURL=main.js.map
--- build/types/processEnv.js
+++
@@ -1,2 +0,0 @@
-export {};
-//# sourceMappingURL=processEnv.js.map
