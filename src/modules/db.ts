e337322e98b532bf2c4f7abebe34bd7910cc3a4a
Арсен <arsendag505@gmail.com>
2023-11-12 00:26:07 +0300

    Update db.ts

--- src/modules/db.ts
+++ src/modules/db.ts
@@ -2,11 +2,9 @@ import pg from 'pg'
 
 const pool = new pg.Pool({
     connectionString: process.env.DB_URL,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    host: process.env.DB_HOST,
-    port: 5432,
-    database: process.env.DB_DATABASE
+    ssl: {
+        rejectUnauthorized: false
+    }
 })
 
 export {pool}
