094203d96a554c20d471bf471382aed1863a47cc
Арсен <arsendag505@gmail.com>
2023-11-12 22:52:53 +0300

    commit

--- src/modules/user-controller.ts
+++ src/modules/user-controller.ts
@@ -10,16 +10,16 @@ const __dirname: string = dirname(__filename);
 export class userController {
     async getUsers(req: Request, res: Response) {
         const q: QueryResult = await pool.query('SELECT * FROM users')
+        console.log(q)
     
-        console.log(q.rows)
         res.render('index.ejs', {lox: 'lox1'})
     }
 
     async logup(req: Request, res: Response) {
-        const {name, password} = req.body
+        console.log(req.body)
 
-        const user: QueryResult = await pool.query('INSERT INTO (name, password, time) VALUES ($1, $2, $3)', [name, password, '11:25'])
-        console.log(user)
+        // const user: QueryResult = await pool.query('INSERT INTO (name, password, time) VALUES ($1, $2, $3)', [name, password, '11:25'])
+        // console.log(user)
     }
 
     async login(req: Request, res: Response) {
